49d6329caf53d3190528c72892c012460c74010b
Jason Goldstein <jason@betheshoe.com>
2016-07-15 17:36:37 -0400

    More stuff

--- beagle.py
+++ beagle.py
@@ -10,46 +10,65 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 from jinja2 import Environment, FileSystemLoader
 
-try:
-    from pync import Notifier
-except ImportError:
-    Notifier = None
+is_dev = "dev" in sys.argv
+
+Notifier = None
+if is_dev:
+    try:
+        from pync import Notifier
+    except ImportError:
+        pass
 
 
-class BaseAction(object):
-    def __init__(*kwargs):
+class Command(object):
+
+    app = None
+    requires = []
+
+    def __init__(self, **kwargs):
+        
+        # Ensure that all required kwargs are present
+        for key in self.requires:
+            if key not in kwargs:
+                raise Exception("Required kwarg missing: %s" % key)
+
+        # Set all kwargs on the class
         for k, v in kwargs.items():
             setattr(self, k, v)
 
-    def set_engine(self, engine):
-        self.engine = engine
+    def set_app(self, app):
+        self.app = app
 
     def render(self):
-        pass
+        raise Exception("You need to implement a render method.")
 
 
-class Page(BaseAction):
+class Page(Command):
     """
     A templating object.
     """
-    def __init__(self, output, template, context):
-        self.output = output
-        self.template = template
-        self.context = context
+    requires = ("template", "context", "output", )
 
-
-
-    def render(self, engine):
-        template = engine.jinja.get_template(self.template)
+    def render(self):
+        template = self.app.jinja.get_template(self.template)
         html = template.render(**self.context)
-        with open(os.path.join(engine.dist, self.output), "w") as f:
+        with open(os.path.join(self.app.dist, self.output), "w") as f:
             f.write(html)
 
 
-class Sass(BaseAction):
+class CopyDir(Command):
+    requires = ["directory", "output"]
+    
+    def render(self):
+        shutil.copy(self.directory, self.output)
+
+
+class Sass(Command):
 
     def render(self):
-        pass
+        sass_input = os.path.join(self.app.src, self.file)
+        sass_output = os.path.join(self.app.dist, self.output)
+        subprocess.call("sassc --sourcemap %s %s" % (sass_input, sass_output), shell=True)
 
 
 def action(func):
@@ -60,21 +79,22 @@ def action(func):
     func._is_beagle_action = True
     return func
 
+server = None
+if is_dev:
 
-# Setup Flask app, which will be our dev server
-from flask import Flask
-app = Flask(__name__, static_folder="dist")
+    # Setup Flask app, which will be our dev server
+    from flask import Flask
+    server = Flask(__name__, static_folder="dist")
+
+    @server.route('/')
+    def index():
+        return server.send_static_file('index.html')
 
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-
-@app.route('/<path:path>')
-def everything_else(path):
-    # send_static_file will guess the correct MIME type
-    return app.send_static_file(path)
+    @server.route('/<path:path>')
+    def everything_else(path):
+        # send_static_file will guess the correct MIME type
+        return server.send_static_file(path)
 
 
 class Engine(object):
@@ -85,9 +105,14 @@ class Engine(object):
         self.dist = dist
 
         # Set flask to serve from the right directory
-        app.static_folder = self.dist
+        if server:
+            server.static_folder = self.dist
 
         self.jinja = Environment(loader=FileSystemLoader(self.src))
+
+        self.render()
+        if is_dev:
+            self.watch()
 
     def do_action(self, action):
         assets = action()
@@ -97,8 +122,8 @@ class Engine(object):
             assets = (assets, )
 
         for asset in assets:
-            asset.set_engine(self)
-            asset.render(self)
+            asset.set_app(self)
+            asset.render()
 
 
     def render(self):
@@ -129,11 +154,6 @@ class Engine(object):
             if hasattr(attr, "_is_beagle_action"):
                 self.do_action(attr)
 
-        for sass in project_index.get("sass", []):
-            sass_input = os.path.join(self.src, sass["file"])
-            sass_output = os.path.join(self.dist, sass["output"])
-            subprocess.call("sassc --sourcemap %s %s" % (sass_input, sass_output), shell=True)
-        
         # js_input = os.path.join(PROJECT_DIR, "static/js/app.js")
         # js_output = os.path.join(PROJECT_DIR, "static/js/app.min.js")
         # subprocess.call("browserify %s -t [ babelify --presets [ es2015 ] ] --output %s" % (js_input, js_output), shell=True)
@@ -170,7 +190,7 @@ class Engine(object):
         try:
             # Run Simple python server
             # run_server()
-            app.run()
+            server.run()
         except KeyboardInterrupt:
             observer.stop()
         observer.join()
